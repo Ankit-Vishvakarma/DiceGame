7a02f932ef9b2fc4ee59f95e753f0408a381a602
Ankit Vishwakarma <ankitvishwakarma1609@gmail.com>
2022-05-14 21:17:13 +0530

    Adding all files

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.floor((Math.random() * 6) + 1); // 1-6 random number number
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1 - dice6
+
+var randomImageSource = "images/" + randomDiceImage; // image dice1 - dice6
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor((Math.random() * 6) + 1); // 1-6 random number number
+
+var randomImageSource2 = "images/" + "dice" + randomNumber2 + ".png"; // image dice1 - dice6
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+// Player 1 win
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🥇 Player 1 Wins!"
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = " Player 2 Wins! 🥇" // Player 2 win
+} else {
+  document.querySelector("h1").innerHTML = "Draw!"
+}
